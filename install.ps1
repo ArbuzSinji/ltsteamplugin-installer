a83b07b2df5ae8fe20da82de0482e1b2cf406342
ArbuzSinji <arturkrausss@gmail.com>
2026-06-24 18:09:39 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-# ZIP
+#  ZIP
 $zipUrl = "https://github.com/piqseu/ltsteamplugin/releases/download/v8.0.5/ltsteamplugin.zip"
 
 Write-Host "Searching for Steam installation..." -ForegroundColor Cyan
@@ -42,19 +42,31 @@ Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 
 Write-Host "Extracting..." -ForegroundColor Cyan
 
-# Распаковываем
+
 Expand-Archive -Path $zipPath -DestinationPath $temp -Force
 
-# Удаляем старую установку
+
+$root = Get-ChildItem -Path $temp | Where-Object { $_.PSIsContainer } | Select-Object -First 1
+
+if (-not $root) {
+    Write-Host "ZIP structure invalid." -ForegroundColor Red
+    exit 1
+}
+
+
 if (Test-Path $millenniumPath) {
     Remove-Item $millenniumPath -Recurse -Force
 }
 
-# Создаём папку и копируем файлы
-New-Item -ItemType Directory -Path $millenniumPath | Out-Null
-Copy-Item -Path "$temp\*" -Destination $millenniumPath -Recurse -Force
 
-# Чистим временные файлы
+if (-not (Test-Path $millenniumPath)) {
+    New-Item -ItemType Directory -Path $millenniumPath | Out-Null
+}
+
+
+Copy-Item -Path (Join-Path $root.FullName "*") -Destination $millenniumPath -Recurse -Force
+
+
 Remove-Item $temp -Recurse -Force
 Remove-Item $zipPath -Force
 
